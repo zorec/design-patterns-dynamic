547a3f8b03152473f3678f4bb03269b0d94e355b
Gabriel <gabo6661@gmail.com>
2015-05-05 13:09:01 +0200

    feat(decorator): transform coffee, sugar and milk into (example of) decorator

--- decorator.rb
+++ decorator.rb
@@ -1,7 +1,9 @@
 # Design pattern: Decorator
-# Description: Attach additional responsibilities to an object dynamically. Decorators provide a flexible alternative to subclassing for extending functionality.
+# Description: Attach additional responsibilities to an object dynamically.
+# Decorators provide a flexible alternative to subclassing for extending functionality.
 
-require 'forwardable'
+# use SimpleDelegator
+require 'delegate'
 
 class Coffee
   def cost
@@ -9,30 +11,20 @@ class Coffee
   end
 end
 
-class Milk
-  extend Forwardable
-  def_delegator :@base, :base_cost, :cost
-
-  def initialize(base)
-    @base = base
-  end
-
+class Milk < SimpleDelegator
   def cost
-    base_cost + 0.4
+    get_base.cost + 0.4
   end
+
+  alias get_base __getobj__
 end
 
-class Sugar
-  extend Forwardable
-  def_delegator :@base, :base_cost, :cost
-
-  def initialize(base)
-    @base = base
-  end
-
+class Sugar < SimpleDelegator
   def cost
-    base_cost + 0.1
+    get_base.cost + 0.1
   end
+
+  alias get_base __getobj__
 end
 
 
@@ -40,30 +32,35 @@ coffee = Coffee.new
 sugar_coffe = Sugar.new(coffee)
 puts sugar_coffe.cost
 # => 2.1
-milk_coffe = Milk.new(coffee)
-puts milk_coffe.cost
+milk_coffee = Milk.new(coffee)
+puts milk_coffee.cost
 # => 2.4
+double_milk = Milk.new(milk_coffee)
+puts double_milk.cost
+# => 2.8
 
-# re-implementation of typical example  e.g https://robots.thoughtbot.com/evaluating-alternative-decorator-implementations-in
-# class Coffee
-#   def cost
-#     2
-#   end
-# end
-#
-# module Milk
-#   def cost
-#     super + 0.4
-#   end
-# end
-#
-# module Sugar
-#   def cost
-#     super + 0.2
-#   end
-# end
-#
-# coffee = Coffee.new
-# coffee.extend(Milk)
-# coffee.extend(Sugar)
-# coffee.cost   # 2.6
+# inspired by https://robots.thoughtbot.com/evaluating-alternative-decorator-implementations-in
+<<-EXAMPLE
+class Coffee
+  def cost
+    2
+  end
+end
+
+module Milk
+  def cost
+    super + 0.4
+  end
+end
+
+module Sugar
+  def cost
+    super + 0.2
+  end
+end
+
+coffee = Coffee.new
+coffee.extend(Milk)
+coffee.extend(Sugar)
+coffee.cost   # 2.6
+EXAMPLE
